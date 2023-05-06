afe3826b3a0067c820c621526a4368cded5ddaaf
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-06 23:06:11 +0200

    Update 1__Menstruationsblutung.py

--- pages/1__Menstruationsblutung.py
+++ pages/1__Menstruationsblutung.py
@@ -3,12 +3,19 @@ import pandas as pd
 import altair as alt
 import json
 
+# -------- load secrets for jsonbin.io --------
+jsonbin_secrets = st.secrets["jsonbin"]
+api_key = jsonbin_secrets["api_key"]
+bin_id = jsonbin_secrets["bin_id"]
 
 st.title("Menstruationsblutung Auswertung")
 
 #barchart with pandas for Menstruationblutung
-with open("data.json", "r")as file:
-    file_intensity = json.load(file)
+
+file_intensity=load_data(api_key, bin_id)
+
+
+
     
 intensity = "intensity"
 bleeding = [day[intensity]for key, day in file_intensity.items() if intensity in day]   #getting values from nested dictionary
