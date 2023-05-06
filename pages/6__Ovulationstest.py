dfeb5787f2945c3380638752e3920b09072c2b3a
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-06 23:14:03 +0200

    Update 6__Ovulationstest.py

--- pages/6__Ovulationstest.py
+++ pages/6__Ovulationstest.py
@@ -1,14 +1,19 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import json
+from jsonbin import load_data, save_data
+
+# -------- load secrets for jsonbin.io --------
+jsonbin_secrets = st.secrets["jsonbin"]
+api_key = jsonbin_secrets["api_key"]
+bin_id = jsonbin_secrets["bin_id"]
 
 
 st.title("Ovulationstest Auswertung")
 
 #barchart with pandas for Ovutest
-with open("data.json", "r")as file:
-    file_ovutest = json.load(file)
+
+file_ovutest=load_data(api_key, bin_id)
     
 ovu = "ovutest"
 ovutests = [day[ovu]for key, day in file_ovutest.items() if ovu in day]   #getting values from nested dictionary
