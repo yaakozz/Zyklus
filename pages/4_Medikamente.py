68b8c3087873f7fbf59e39a3636c22383c55e120
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-06 23:37:45 +0200

    Add files via upload

--- pages/4_Medikamente.py
+++ pages/4_Medikamente.py
@@ -7,29 +7,27 @@ Created on Mon May  1 18:04:36 2023
 
 import streamlit as st
 import pandas as pd
-from jsonbin import load_data, save_data
-
-# -------- load secrets for jsonbin.io --------
-jsonbin_secrets = st.secrets["jsonbin"]
-api_key = jsonbin_secrets["api_key"]
-bin_id = jsonbin_secrets["bin_id"]
+import json
 
 #Dataframe with pandas for medis
-
-file_medi1=load_data(api_key, bin_id)
+with open("data.json", "r")as file: 
+    file_medi1 = json.load(file)
 
 medi1 = "medi1"
 Morgen = [day[medi1]for key, day in file_medi1.items() if medi1 in day]     #getting values from nested dictionary
 
-Mittag = [day[medi1]for key, day in file_medi1.items() if medi1 in day]
+medi2 = "medi2"
+Mittag = [day[medi2]for key, day in file_medi1.items() if medi2 in day]
 
-Abend = [day[medi1]for key, day in file_medi1.items() if medi1 in day]
+medi3 = "medi3"
+Abend = [day[medi3]for key, day in file_medi1.items() if medi3 in day]
 
-Schlafen = [day[medi1]for key, day in file_medi1.items() if medi1 in day]
+medi4 = "medi4"
+Schlafen = [day[medi4]for key, day in file_medi1.items() if medi4 in day]
 
-Datum = file_medi1.keys()
+Tag = file_medi1.keys()
 
-taken_medi = dict((k, eval(k)) for k in ("Morgen", "Mittag", "Abend", "Schlafen", "Datum"))     #creating dictionary for panda dataframe
+taken_medi = dict((k, eval(k)) for k in ("Morgen", "Mittag", "Abend", "Schlafen", "Tag"))     #creating dictionary for panda dataframe
 df = pd.DataFrame.from_dict(taken_medi, orient = "columns")
 
 st.dataframe(df, width=1024, height=768)
