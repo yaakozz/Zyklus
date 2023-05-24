284325a0f12258fe8702a00e2333b31cc0f905c6
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 10:47:10 +0200

    Update 7__Temperatur.py

--- pages/7__Temperatur.py
+++ pages/7__Temperatur.py
@@ -1,19 +1,50 @@
-import streamlit as st
-import pandas as pd
 import altair as alt
-from jsonbin import load_data, save_data
+import streamlit as st
+from datetime import datetime, date, timedelta 
+from jsonbin import load_key, save_key     #from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+import pandas as pd
 
 # -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
 api_key = jsonbin_secrets["api_key"]
 bin_id = jsonbin_secrets["bin_id"]
 
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+
 
 st.title("Temperatur Auswertung")
 
 #bar chart for temperature
 
-file_temperature=load_data(api_key, bin_id)
+file_temperature=load_key(api_key, bin_id, username)
+
+Leer={}
+for key in file_temperatur:
+    Leer.update(key)
+file_temperatur=Leer
         
     #all measured Temperatures
 temp = "temperatur"
@@ -33,7 +64,7 @@ df = pd.DataFrame({
     "day": file_temperature.keys()
 })
 
-st.write("Gemessene Werte", temp_not_none(temperatures))
+#st.write("Gemessene Werte", temp_not_none(temperatures))
 
 
 bar_chart = alt.Chart(df).mark_bar().encode(
