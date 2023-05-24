466c1dec41a6db26a054c6b7bea250d699e3f050
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 10:06:07 +0200

    Update 1__Menstruationsblutung.py

--- pages/1__Menstruationsblutung.py
+++ pages/1__Menstruationsblutung.py
@@ -44,7 +44,7 @@ file_intensity=load_key(api_key, bin_id, username)
 
 for key in file_intensity:
     st.write(key.values())
-    st.write(key["intensity"])
+    st.write(key.values["intensity"])
     
 intensity = "intensity"
 bleeding = [day[intensity]for key, day in file_intensity.items() if intensity in day]   #getting values from nested dictionary
