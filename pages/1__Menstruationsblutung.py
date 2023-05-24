e6e08aef5a051f38368be825cbd27c98226a0221
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 08:26:00 +0200

    Update 1__Menstruationsblutung.py

--- pages/1__Menstruationsblutung.py
+++ pages/1__Menstruationsblutung.py
@@ -1,19 +1,44 @@
-import streamlit as st
-import pandas as pd
 import altair as alt
-from jsonbin import load_data, save_data
+import streamlit as st
+from datetime import datetime, date, timedelta 
+from jsonbin import load_key, save_key     #from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
 
 # -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
 api_key = jsonbin_secrets["api_key"]
 bin_id = jsonbin_secrets["bin_id"]
 
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+
+
 st.title("Menstruationsblutung Auswertung")
 
 #barchart with pandas for Menstruationblutung
 
 file_intensity=load_data(api_key, bin_id)
-
 
 
     
