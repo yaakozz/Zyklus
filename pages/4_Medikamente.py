2225bd24997493d387aac9efcc924aef77fb75c8
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 12:55:12 +0200

    Update 4_Medikamente.py

--- pages/4_Medikamente.py
+++ pages/4_Medikamente.py
@@ -17,6 +17,9 @@ bin_id = jsonbin_secrets["bin_id"]
 #Dataframe with pandas for medis
 
 file_medi1=load_data(api_key, bin_id)
+dfML=pd.DataFrame(file_medi1)
+st.dataframe(dfML)
+
 
 medi1 = "medi1"
 Morgen = [day[medi1]for key, day in file_medi1.items() if medi1 in day]     #getting values from nested dictionary
