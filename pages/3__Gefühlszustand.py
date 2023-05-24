83a51eb9c59ac142b7fb7744e6c05bd988974be2
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 10:39:49 +0200

    Update 3__Gefühlszustand.py

--- pages/3__Gefühlszustand.py
+++ pages/3__Gefühlszustand.py
@@ -1,20 +1,51 @@
-import streamlit as st
-import pandas as pd
 import altair as alt
-from jsonbin import load_data, save_data
+import streamlit as st
+from datetime import datetime, date, timedelta 
+from jsonbin import load_key, save_key     #from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+import pandas as pd
 
 # -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
 api_key = jsonbin_secrets["api_key"]
 bin_id = jsonbin_secrets["bin_id"]
 
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+
+
 
 st.title("Gefühlszustand Auswertung")
 
 #barchart with pandas for feelings
 
-file_feeling=load_data(api_key, bin_id)
+file_feeling=load_key(api_key, bin_id, username)
 
+Leer={}
+for key in file_feeling:
+    Leer.update(key)
+file_feeling=Leer
 
 feel = "feeling"
 feelings = [day[feel]for key, day in file_feeling.items() if feel in day]   #getting values from nested dictionary
@@ -24,6 +55,6 @@ dif = pd.DataFrame({
     "day" : file_feeling.keys()
     })
 
-st.write("Gemessene Werte", feelings)
+#st.write("Gemessene Werte", feelings)
 
 st.line_chart(dif, x= "day", y = "feeling")
