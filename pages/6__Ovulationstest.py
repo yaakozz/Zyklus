fb9cc3263510122f2b27905b3748ec068f1e71b9
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 10:45:05 +0200

    Update 6__Ovulationstest.py

--- pages/6__Ovulationstest.py
+++ pages/6__Ovulationstest.py
@@ -1,19 +1,50 @@
-import streamlit as st
-import pandas as pd
 import altair as alt
-from jsonbin import load_data, save_data
+import streamlit as st
+from datetime import datetime, date, timedelta 
+from jsonbin import load_key, save_key     #from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+import pandas as pd
 
 # -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
 api_key = jsonbin_secrets["api_key"]
 bin_id = jsonbin_secrets["bin_id"]
 
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
 
 st.title("Ovulationstest Auswertung")
 
 #barchart with pandas for Ovutest
 
-file_ovutest=load_data(api_key, bin_id)
+file_ovutest=load_key(api_key, bin_id, username)
+
+Leer={}
+for key in file_ovutest:
+    Leer.update(key)
+file_ovutest=Leer
+    
     
 ovu = "ovutest"
 ovutests = [day[ovu]for key, day in file_ovutest.items() if ovu in day]   #getting values from nested dictionary
@@ -33,7 +64,7 @@ daf = pd.DataFrame({
     "day" : file_ovutest.keys()
     })
 
-st.write("Gemessene Werte", measured_ovu)
+#st.write("Gemessene Werte", measured_ovu)
 
 bar_chart = alt.Chart(daf).mark_bar().encode(
     x = "day",
