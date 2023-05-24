e16345a6087aaea7e39c61481d1cea6b1afc867e
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 10:11:11 +0200

    Update 1__Menstruationsblutung.py

--- pages/1__Menstruationsblutung.py
+++ pages/1__Menstruationsblutung.py
@@ -43,7 +43,7 @@ file_intensity=load_key(api_key, bin_id, username)
 
 
 for key in file_intensity:
-    df=pd.DataFrame(key.values()).T
+    df=pd.DataFrame(key).T
     st.write(df)
     
 intensity = "intensity"
