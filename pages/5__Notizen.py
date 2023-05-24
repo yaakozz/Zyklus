a75a15b5d8613837ea6429b6c1180f1d6336276c
yaakozz <131615173+yaakozz@users.noreply.github.com>
2023-05-24 10:42:22 +0200

    Update 5__Notizen.py

--- pages/5__Notizen.py
+++ pages/5__Notizen.py
@@ -1,16 +1,49 @@
+import altair as alt
 import streamlit as st
+from datetime import datetime, date, timedelta 
+from jsonbin import load_key, save_key     #from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
 import pandas as pd
-from jsonbin import load_data, save_data
 
 # -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
 api_key = jsonbin_secrets["api_key"]
 bin_id = jsonbin_secrets["bin_id"]
 
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+
+
 
 st.title("Notizen Auswertung")
 
-file_notice=load_data(api_key, bin_id)
+file_notice=load_key(api_key, bin_id, username)
+
+Leer={}
+for key in file_notice:
+    Leer.update(key)
+file_notice=Leer
     
 notice = "notice"
 notes = [day[notice]for key, day in file_notice.items() if notice in day]   #getting values from nested dictionary
